9c295ede42c1b824702cf18111a86d532e930029
Michael <mackelldeveloper@gmail.com>
2020-08-26 00:52:07 -0300

    Usando JavaScript para manipular o DOM

---
+++ DOM/scripts/scripts.js
@@ -0,0 +1,28 @@
+/* 
+var p1 = document.getElementsByTagName('p')[1]
+document.write(p1.innerHTML)
+p1.style.color = 'yellow'
+window.alert(p1.innerText)
+*/
+
+var p1 = document.getElementsByTagName('p')[1]
+p1.style.color = 'yellow'
+
+/*
+var msg = document.getElementById('msg')
+msg.style.background = '#31B404'
+msg.innerText = 'Carregando...'
+
+var msg = document.getElementsByName('msg')[0]
+msg.style.background = 'red'
+msg.innerText = 'Falha ao baixar arquivo...'
+
+*/
+
+/* Selecionar usando o querySelector */
+
+/*
+let msg = document.querySelector('#msg')
+msg.style.background = 'yellow'
+msg.style.color = 'black'
+*/
