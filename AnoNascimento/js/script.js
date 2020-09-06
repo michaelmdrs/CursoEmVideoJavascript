0f00ae62660c5ef7e71cf1a25f324477ebb2f9fc
michaelmdrs <mackelldeveloper@gmail.com>
2020-09-05 21:50:54 -0300

    refatoração do código

--- AnoNascimento/js/script.js
+++ AnoNascimento/js/script.js
@@ -1,18 +1,19 @@
 function verificar() {
   var data = new Date();
-  var ano = data.getFullYear();
+  var ano = data.getFullYear(); //captura o ano com 4 dígitos
   var ano_usuario = document.getElementById("txtano");
-  var res = document.getElementById("res");
+  var res = document.querySelector('div#res');
 
   if (ano_usuario.value.length == 0 || Number(ano_usuario.value) > ano) {
     alert("[ERRO] Verifique os dados e tente novamente");
   } else {
+    
     var form_sex = document.getElementsByName("radsex");
     var idade = ano - Number(ano_usuario.value);
-    //res.innerHTML = `Idade calculada ${idade}`
     var genero = "";
     var img = document.createElement("img");
     img.setAttribute("id", "foto");
+
     if (form_sex[0].checked) {
       genero = "Homem";
       if (idade >= 0 && idade < 12) {
@@ -38,7 +39,7 @@ function verificar() {
     }
 
     res.style.textAlign = "center";
-    res.innerHTML = `Detectamos ${genero} de ${idade} anos.`;
-    res.appendChild(img);
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+    res.appendChild(img)
   }
 }
