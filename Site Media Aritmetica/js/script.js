ebfcb0a69d8a44b4b88bd2370703ae6f75423a16
Michael <mackelldeveloper@gmail.com>
2020-09-11 01:19:35 -0300

    Preparação para resolução dos exercicios

--- Site Media Aritmetica/js/script.js
+++ Site Media Aritmetica/js/script.js
@@ -1,15 +1,17 @@
 function verificar() {
     var nome = document.getElementById('txtnome')
-    var nota1 = document.getElementById('nota1')
-    var nota2 = document.getElementById('nota2')
-    var nota3 = document.getElementById('nota3')
-    var nota4 = document.getElementById('nota4')
+    var nota1 = Number(document.getElementById('nota1').value)
+    var nota2 = Number(document.getElementById('nota2').value)
+    var nota3 = Number(document.getElementById('nota3').value)
+    var nota4 = Number(document.getElementById('nota4').value)
+
     var media = Number(nota1 + nota2 + nota3 + nota4) / 4
-    var resultado = document.getElementById('resultado')
+
+    var resultado = document.getElementById('resultado').setAttribute('value', resultado)
 
     if (nome.value.length == 0) {
         alert('[ATENÇÃO]! Preencha o campo corretamente e tente novamente.')
-    }else{
+    } else {
         var sexo = document.getElementsByName('radsex')
         var genero = ''
         var img = document.createElement('img')
@@ -18,28 +20,28 @@ function verificar() {
             genero = 'Homem'
             if (media >= 10) {
                 img.setAttribute('src', 'assets/emoji_nerd.png')
-            }else if (media >= 7 || media <= 9) {
+            } else if (media >= 7 || media <= 9) {
                 img.setAttribute('src', 'assets/emoji_aprovado.png')
-            }else if (media >= 5 || media < 7) {
+            } else if (media >= 5 || media < 7) {
                 img.setAttribute('src', 'assets/emoji_reprovado.png')
-            }else {
+            } else {
                 img.setAttribute('src', 'assets/emoji_reprovado.png')
             }
-        }else if (sexo[1].checked) {
+        } else if (sexo[1].checked) {
             genero = 'Mulher'
             if (media >= 10) {
                 img.setAttribute('src', 'assets/emoji_nerd.png')
-            }else if (media >= 7 || media <= 9) {
+            } else if (media >= 7 || media <= 9) {
                 img.setAttribute('src', 'assets/emoji_aprovado.png')
-            }else if (media >= 5 || media < 7) {
+            } else if (media >= 5 || media < 7) {
                 img.setAttribute('src', 'assets/emoji_reprovado.png')
-            }else {
+            } else {
                 img.setAttribute('src', 'assets/emoji_reprovado.png')
             }
         }
 
         resultado.style.textAlign = 'center'
-        resultado.innerHTML = `REPROVADO ${media}`
+        resultado.innerHTML = `MEDIA ${media}`
         resultado.appendChild(img)
     }
 }
