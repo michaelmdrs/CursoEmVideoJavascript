0761bb914beb815a9775970219c5a368749f9b86
Michael <mackelldeveloper@gmail.com>
2020-09-13 00:31:34 -0300

    Refatorando o CSS e implementando a lógica

---
+++ aula14exe/js/script.js
@@ -0,0 +1,10 @@
+function verificar() {
+    var inicio = Number(document.getElementById('inicio').value)
+    var fim = Number(document.getElementById('fim').value)
+    var passo = Number(document.getElementById('passo').value)
+    var res = document.getElementById('res')
+
+    if (inicio < 0) {
+        document.write('Valor inválido.')
+    } 
+}
