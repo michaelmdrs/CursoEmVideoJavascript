d76464b74d4c854cc76ce1aef3021ea656d5d565
Michael <mackelldeveloper@gmail.com>
2020-09-13 03:03:46 -0300

    Refatoração do código

--- aula14exe/js/script.js
+++ aula14exe/js/script.js
@@ -4,7 +4,27 @@ function verificar() {
     var passo = Number(document.getElementById('passo').value)
     var res = document.getElementById('res')
 
-    if (inicio < 0) {
-        document.write('Valor inválido.')
-    } 
+    if (inicio == 0 || fim == 0 || passo == 0) {
+        alert('[ERROR] digite os valores!')
+        res.innerHTML = 'Impossível contar'
+    }else {
+        res.innerHTML = 'Contando: <br>'
+
+        if (passo <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            passo = 1
+        }
+        // Contagem crescente
+        if(inicio < fim) {
+            for (var count = inicio; count <= fim; count += passo) {
+                res.innerHTML += `${count} \u{1F449}`
+            }
+        }else {
+            // Contagem regressiva
+            for (var count = inicio; count >= fim; count -= passo) {
+                res.innerHTML += `${count} \u{1F449}`
+            }
+        }
+        res.innerHTML += ` \u{1F3C1}`
+    }
 }
