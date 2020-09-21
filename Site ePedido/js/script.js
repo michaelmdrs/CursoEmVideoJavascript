53c77d859fa7e80d39eb4dc593f6e3e0b047a396
Michael <mackelldeveloper@gmail.com>
2020-09-20 22:41:44 -0300

    Pagina Pedido

---
+++ Site ePedido/js/script.js
@@ -0,0 +1,8 @@
+// Variaveis para desenvolver o escopo do código.
+let codProduto = Number(document.querySelector('input#codProduto'))
+let txtPedido = document.querySelector('input#txtpedido')
+let descPedido = document.querySelector('textarea#pedido')
+
+function adicionar() {
+    
+}
