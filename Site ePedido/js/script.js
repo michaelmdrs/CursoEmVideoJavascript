68088dae2a5f77cb5ffbfa31da9f7b25deff5b53
Michael <mackelldeveloper@gmail.com>
2020-09-22 00:06:32 -0300

    Pagina de Pedido
    Refatoração do código da página

--- Site ePedido/js/script.js
+++ Site ePedido/js/script.js
@@ -1,8 +1,19 @@
 // Variaveis para desenvolver o escopo do código.
-let codProduto = Number(document.querySelector('input#codProduto'))
-let txtPedido = document.querySelector('input#txtpedido')
+let codProduto = document.querySelector('input#codProduto')
+let descProduto = document.querySelector('input#txtPedido')
 let descPedido = document.querySelector('textarea#pedido')
+let resPedido = document.querySelector('div#res')
+let valores = []
 
 function adicionar() {
+    if (Number(codProduto.value) && Number(codProduto.value), valores) {
+        descPedido.innerHTML = ''
+        descPedido.innerHTML += `Item ${descProduto.value} | Cod: ${codProduto.value}, adicionado.`
+    } else {
+        alert('O código do produto é inválido ou já consta na lista!')
+    }
+}
+
+function enviarPedido() {
     
 }
